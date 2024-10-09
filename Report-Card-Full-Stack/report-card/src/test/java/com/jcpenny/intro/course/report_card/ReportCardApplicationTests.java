5ce35ca4778f6513c42c1ed973404eb4262e81e7
JCPennyWorth <jay.e.clark96@gmail.com>
2024-10-08 21:53:57 -0500

    initialize all projects

---
+++ Report-Card-Full-Stack/report-card/src/test/java/com/jcpenny/intro/course/report_card/ReportCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcpenny.intro.course.report_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
